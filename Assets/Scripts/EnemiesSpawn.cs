74dec65b8e095493f95dcf83018dee6716b56ddb
Daniel <daniel@dancm.net>
2023-04-22 22:28:47 +0200

    dev: dynamic multiplier synchronisation entre ennemis et planète

--- Assets/Scripts/EnemiesSpawn.cs
+++ Assets/Scripts/EnemiesSpawn.cs
@@ -17,6 +17,8 @@ public class EnemiesSpawn : MonoBehaviour
     private List<EnemyPosition> instances = new List<EnemyPosition>();
     private float timerElapsed = 0;
 
+    public ScoreCount counter;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,21 +29,29 @@ public class EnemiesSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         timerElapsed += Time.deltaTime;
+
+        float multiplier = ScoreCount.Instance.GetMultiplier();
+
+        if (multiplier > 999)
+        {
+            multiplier = 999;
+        }
+
+        float multipliedSpeed = speed + (multiplier * 2 / 100);
+
+        //Debug.Log("Enemies:");
+        //Debug.Log(multipliedSpeed);
 
         if (timerElapsed > frequence)
         {
             if (enemies.Count > 0)
             {
                 int index = UnityEngine.Random.Range(0, enemies.Count);
-
                 GameObject newEnemyObject = Instantiate(enemies[index], transform);
-
-                EnemyPosition enemy = new EnemyPosition(newEnemyObject, transform.position, radius, speed);
+                EnemyPosition enemy = new EnemyPosition(newEnemyObject, transform.position, radius, multipliedSpeed);
 
                 instances.Add(enemy);
-
             }
             timerElapsed = 0;
         }
@@ -49,7 +59,6 @@ public class EnemiesSpawn : MonoBehaviour
         for (int i = 0; i < instances.Count; i++)
         {
             EnemyPosition enemy = instances[i];
-
 
             if (enemy.IsGtRadius(Mathf.PI) )
             {
@@ -61,10 +70,6 @@ public class EnemiesSpawn : MonoBehaviour
             {
                 enemy.Update();
             }
-
-            
-
         }
-
     }
 }
