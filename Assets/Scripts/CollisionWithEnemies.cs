e060823f2c63b608d523602f3d825786d3eadbee
sophia <sophia.koulen@outlook.com>
2023-04-22 17:41:24 +0200

    Created unity event onPLayerDeath

--- Assets/Scripts/CollisionWithEnemies.cs
+++ Assets/Scripts/CollisionWithEnemies.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* maybe not here ?*/
+using UnityEngine.SceneManagement;
+
+using UnityEngine.Events;
+
 public class CollisionWithEnemies : MonoBehaviour
 {
 	public LayerMask enemyLayerMask;
+	public UnityEvent onPlayerDeath;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +30,7 @@ public class CollisionWithEnemies : MonoBehaviour
 		if (((1 << col.gameObject.layer) & enemyLayerMask.value) != 0)
 		{
 			Debug.Log("hello");
+			onPlayerDeath.Invoke();
 		}
 	}
 }
