be3a53b9967a566c9ab29101d893213af96ef3ed
Daniel <daniel@dancm.net>
2023-04-22 02:27:43 +0200

    dev: mic input webgl & non-webgl
    Mostly working but still some non blocking errors

--- Assets/Scripts/AudioLoudnessDetection.cs
+++ Assets/Scripts/AudioLoudnessDetection.cs
@@ -8,37 +8,48 @@ public class AudioLoudnessDetection : MonoBehaviour
 
     private AudioClip microphoneClip;
 
-#if UNITY_WEBGL && !UNITY_EDITOR
+    #if UNITY_WEBGL && !UNITY_EDITOR
     void Awake()
     {
         Microphone.Init();
         Microphone.QueryAudioInput();
     }
-#endif
+    #endif
 
     // Start is called before the first frame update
     void Start()
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+#else
         MicrophoneToAudioClip();
+#endif
     }
 
     // Update is called once per frame
     void Update()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
+        #if UNITY_WEBGL && !UNITY_EDITOR
         Microphone.Update();
-#endif
+        #endif
     }
 
+#if UNITY_WEBGL && !UNITY_EDITOR
+#else
     public void MicrophoneToAudioClip()
     {
         string microphoneName = Microphone.devices[0];
         microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
     }
+#endif
 
     public float GetLoudnessFromMicrophone()
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        return Microphone.volumes[0];
+#else
+        string microphoneName = Microphone.devices[0];
         return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+#endif
     }
 
     public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
