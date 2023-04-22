4ece70398f6913f06c94b08765d39b3028ce42c3
Daniel <daniel@dancm.net>
2023-04-22 08:35:49 +0200

    dev: cleanup loudness detection

--- Assets/Scripts/AudioLoudnessDetection.cs
+++ Assets/Scripts/AudioLoudnessDetection.cs
@@ -19,37 +19,35 @@ public class AudioLoudnessDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-#else
+    #if !UNITY_WEBGL || UNITY_EDITOR
         MicrophoneToAudioClip();
-#endif
+    #endif
     }
 
     // Update is called once per frame
     void Update()
     {
-        #if UNITY_WEBGL && !UNITY_EDITOR
+    #if UNITY_WEBGL && !UNITY_EDITOR
         Microphone.Update();
-        #endif
+    #endif
     }
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-#else
     public void MicrophoneToAudioClip()
     {
+    #if !UNITY_WEBGL || UNITY_EDITOR
         string microphoneName = Microphone.devices[0];
         microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    #endif
     }
-#endif
 
     public float GetLoudnessFromMicrophone()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
+    #if UNITY_WEBGL && !UNITY_EDITOR
         return Microphone.volumes[0];
-#else
+    #else
         string microphoneName = Microphone.devices[0];
         return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
-#endif
+    #endif
     }
 
     public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
