6463c055cc6151d7588a3f3f8bd6b32cd1d74e59
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-04-23 09:04:16 +0200

    anim control

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -21,7 +21,16 @@ public class JumpController : MonoBehaviour
 
 	public float maxLoudness = 100;
 
-	void Awake()
+	//plazer animation states
+
+	Animator _animator;
+	string _currentState;
+	const string RUNNING = "running";
+	const string JUMPINGSTART = "JumpingStart";
+    const string JUMPINGTOP = "JumpingTop";
+    const string JUMPINGEND = "JumpingEnd";
+
+    void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
 		cc = GetComponent<CapsuleCollider2D>();
@@ -30,22 +39,40 @@ public class JumpController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		
+		_animator = gameObject.GetComponent<Animator>();	
     }
+
 
     // Update is called once per frame
     void Update()
     {
 		if (isGrounded())
 		{
-			/* jump with microphone */
+            if (isAnimationPlaying(_animator, JUMPINGTOP))
+            {
+                ChangeAnimationState(JUMPINGEND);
+            }
+            if (isAnimationPlaying(_animator, JUMPINGEND))
+			{
+				ChangeAnimationState(RUNNING);
+			}
+			else
+                ChangeAnimationState(RUNNING);
+            /* jump with microphone */
 
-			float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
+            float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
 			if (loudness >= threshold)
 			{
 				//Debug.Log(loudness);
 				rb.velocity = Vector2.up * (minJump + (maxJump - minJump) * loudness / maxLoudness);
-			}
+				ChangeAnimationState(JUMPINGSTART);
+                if (isAnimationPlaying(_animator, JUMPINGSTART))
+                {
+                    ChangeAnimationState(JUMPINGTOP);
+                }
+				else
+                    ChangeAnimationState(JUMPINGTOP);
+            }
 
 			/* jump with mouse click: */
 
@@ -66,9 +93,34 @@ public class JumpController : MonoBehaviour
 		}
     }
 
-	public bool isGrounded()
+
+	bool isGrounded()
 	{
 		return Physics2D.Raycast(cc.bounds.center, Vector2.down, cc.bounds.extents.y + 0.1f, groundLayerMask);
 	}
+
+	// change animation state
+
+	private void ChangeAnimationState(string newState)
+	{
+		if (newState == _currentState)
+			return;
+		_animator.Play(newState);
+		_currentState = newState;
+	}
+
+	// check if a specific animation is playing
+	// Parameter named "0" is the animation layer
+
+	bool isAnimationPlaying(Animator animator, string stateName)
+	{
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+			animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+		{
+			return true;
+		}
+		else
+			return false;
+    }
 
 }
